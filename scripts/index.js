41220ed6bcaa1afc7cf183e8dccb5d9c49b59489
Olga Voronina <lelkagrishakova@yandex.ru>
2023-02-03 10:57:45 +0300

    Подключение фото

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 // карточки мест
 const initialCards = [
-  { name: 'Адыгея', link: '../images/adigeya.jpg' },
-  { name: 'Алтай', link: '../images/altai.jpg' },
-  { name: 'Камчатка', link: '../images/kamchatka.jpg' },
-  { name: 'Мурманск', link: '../images/murmansk.jpg' },
-  { name: 'Шерегеш', link: '../images/sheregesh.jpg' },
-  { name: 'Сибирь', link: '../images/siberia.jpg' },
+  { name: 'Адыгея', link: 'images/adigeya.jpg' },
+  { name: 'Алтай', link: 'images/altai.jpg' },
+  { name: 'Камчатка', link: 'images/kamchatka.jpg' },
+  { name: 'Мурманск', link: 'images/murmansk.jpg' },
+  { name: 'Шерегеш', link: 'images/sheregesh.jpg' },
+  { name: 'Сибирь', link: 'images/siberia.jpg' },
 ];
 
 // Листинг карточек мест 
