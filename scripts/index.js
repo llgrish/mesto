c24e4ade31242af2b430410b2f00d0074a2e6f74
Olga Voronina <lelkagrishakova@yandex.ru>
2023-02-03 11:03:39 +0300

    повторная попытка отображать фото

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 // карточки мест
 const initialCards = [
-  { name: 'Адыгея', link: 'images/adigeya.jpg' },
-  { name: 'Алтай', link: 'images/altai.jpg' },
-  { name: 'Камчатка', link: 'images/kamchatka.jpg' },
-  { name: 'Мурманск', link: 'images/murmansk.jpg' },
-  { name: 'Шерегеш', link: 'images/sheregesh.jpg' },
-  { name: 'Сибирь', link: 'images/siberia.jpg' },
+  { name: 'Адыгея', link: 'https://llgrish.github.io/images/adigeya.jpg' },
+  { name: 'Алтай', link: 'https://llgrish.github.io/images/altai.jpg' },
+  { name: 'Камчатка', link: 'https://llgrish.github.io/images/kamchatka.jpg' },
+  { name: 'Мурманск', link: 'https://llgrish.github.io/images/murmansk.jpg' },
+  { name: 'Шерегеш', link: 'https://llgrish.github.io/images/sheregesh.jpg' },
+  { name: 'Сибирь', link: 'https://llgrish.github.io/images/siberia.jpg' },
 ];
 
 // Листинг карточек мест 
