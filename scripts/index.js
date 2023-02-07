06c084ac6fc0906886ed3ee86868c49f50fd576d
Olga Voronina <lelkagrishakova@yandex.ru>
2023-02-07 10:20:42 +0300

    fixed bugs from second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -7,23 +7,22 @@ let titleProfile = profile.querySelector('.profile__name')
 let activityProfile = profile.querySelector('.profile__activity')
 
 let formField = popup.querySelector('.form')
-let nameInput = formField.querySelector('.form__field_name_main')
-let activityInput = formField.querySelector('.form__field_activity_about')
+let nameInput = formField.querySelector('.form__field_profile_name')
+let activityInput = formField.querySelector('.form__field_profile_activity')
 
-openPopupButton.addEventListener('click', OpenForm)
-closePopupButton.addEventListener('click', CloseForm)
+openPopupButton.addEventListener('click', openForm)
+closePopupButton.addEventListener('click', closeForm)
 formField.addEventListener('submit', formSubmitHandler);
 
-function OpenForm() {
+function openForm() {
   popup.classList.toggle("popup__opened")
   nameInput.value = titleProfile.textContent
   activityInput.value = activityProfile.textContent;
 }
 
 
-function CloseForm() {
-  popup.classList.toggle("popup_opened");
-  OpenForm()
+function closeForm() {
+  popup.classList.toggle("popup__opened");
 }
 
 
@@ -31,5 +30,5 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   titleProfile.textContent = nameInput.value;
   activityProfile.textContent = activityInput.value
-  OpenForm()
+  closeForm()
 }
