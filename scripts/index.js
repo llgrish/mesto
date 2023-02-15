f1884b22296b2fc17511405b66a7dcd924e16f37
Olga Voronina <lelkagrishakova@yandex.ru>
2023-02-15 12:22:10 +0300

    ПР 5 на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,73 +1,56 @@
 let initialCards = [
-  {
-    name: 'Адыгея',
-    link: './images/adigeya.jpg'
-  },
-  {
-    name: 'Алтай',
-    link: './images/altai.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: './images/kamchatka.jpg'
-  },
-  {
-    name: 'Мурманск',
-    link: './images/murmansk.jpg'
-  },
-  {
-    name: 'Шерегеш',
-    link: './images/sheregesh.jpg'
-  },
-  {
-    name: 'Сибирь',
-    link: './images/siberia.jpg'
-  }
+  { name: 'Адыгея',
+    link: './images/adigeya.jpg' },
+  { name: 'Алтай',
+    link: './images/altai.jpg' },
+  { name: 'Камчатка',
+    link: './images/kamchatka.jpg' },
+  { name: 'Мурманск',
+    link: './images/murmansk.jpg' },
+  { name: 'Шерегеш',
+    link: './images/sheregesh.jpg' },
+  { name: 'Сибирь',
+    link: './images/siberia.jpg' }
 ];
 
 let popup = document.querySelector('.popup')
+let popupProfile = document.querySelector('.popup_profile')
+let popupPhotoPlace = document.querySelector('.popup_new-place')
+let popupPlaceImage = document.querySelector('.popup_place-image')
+
 let openPopupButton = document.querySelector('.profile__edit-button')
 let openPopupAddButton = document.querySelector('.profile__add-button')
 
-let popupProfile = document.querySelector('.popup_profile')
+
 let closePopupProfileButton = popupProfile.querySelector('.popup__close')
-let formFieldProfile = popupProfile.querySelector('.form')
-let nameInput = formFieldProfile.querySelector('.form__field_profile_name')
-let activityInput = formFieldProfile.querySelector('.form__field_profile_activity')
-
-let popupPhotoPlace = document.querySelector('.popup_new-place')
 let closePopupPlaceButton = popup.querySelector('.popup__close')
-let formFieldPlace = popup.querySelector('.form')
-let pointInput = formFieldPlace.querySelector('.form__field_point')
-let urlInput = formFieldPlace.querySelector('.form__field_url')
-
-let popupPlaceImage = document.querySelector('.popup_place-image')
-let placeImageImage = popupPlaceImage.querySelector('.place-image__image')
-let placeImageName = popupPlaceImage.querySelector('.place-image__name')
 let closePopupImageButton = popupPlaceImage.querySelector('.popup__close')
 
 let profile = document.querySelector('.content')
 let nameProfile = profile.querySelector('.profile__name')
 let activityProfile = profile.querySelector('.profile__activity')
+let formFieldProfile = popupProfile.querySelector('.form')
+let formFieldPlace = popup.querySelector('.form')
+
+let nameInput = formFieldProfile.querySelector('.form__field_profile_name')
+let activityInput = formFieldProfile.querySelector('.form__field_profile_activity')
+let pointInput = formFieldPlace.querySelector('.form__field_point')
+let urlInput = formFieldPlace.querySelector('.form__field_url')
+
+
+let placeImageImage = popupPlaceImage.querySelector('.place-image__image')
+let placeImageName = popupPlaceImage.querySelector('.place-image__name')
 let placesList = document.querySelector('.places__list')
 let template = document.querySelector('.template')
 
-function togglePopup (pop) {
-  pop.classList.toggle('popup__opened');
+
+renderPlaces();
+
+function renderPlaces() {
+  let places = initialCards.map(getPlace);
+  placesList.append(...places);
 }
 
-function upProfileInfo() {
-  nameInput.value = nameProfile.textContent
-  activityInput.value = activityProfile.textContent;
-}
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  nameProfile.textContent = nameInput.value;
-  activityProfile.textContent = activityInput.value;
-
-  togglePopup(popupProfile);
-}
 
 function getPlace(item) {
   let newPlace = template.content.cloneNode(true);
@@ -88,38 +71,28 @@ function getPlace(item) {
     placeImageName.textContent = item.name;
     placeImageImage.src = item.link;
     placeImageImage.alt = item.name;
-
     togglePopup(popupPlaceImage);
   });
 
   return newPlace;
 }
 
+function deletePlace(event) {
+  let targetElement = event.target;
+  let targetItem = targetElement.closest('.place');
+  targetItem.remove();
+}
+
+
 function formPlaceHandler (evt) {
   let pointPlace = pointInput.value;
   let urlPlace = urlInput.value;
   let photoPlace = getPlace({name: pointPlace, link: urlPlace});
-
   evt.preventDefault();
   placesList.prepend(photoPlace);
   pointInput.value = '';
   urlInput.value = '';
-
   togglePopup(popupPhotoPlace);
-}
-
-function renderPlaces() {
-  let places = initialCards.map(getPlace);
-
-  placesList.append(...places);
-}
-
-
-function deletePlace(event) {
-  let targetElement = event.target;
-  let targetItem = targetElement.closest('.place');
-
-  targetItem.remove();
 }
 
 function likePlace(event) {
@@ -127,17 +100,8 @@ function likePlace(event) {
   targetElement.classList.toggle('place__like_active');
 }
 
-renderPlaces();
-
-openPopupButton.addEventListener('click',() => {
-  upProfileInfo();
-  togglePopup(popupProfile);
-});
 openPopupAddButton.addEventListener('click',() => {
   togglePopup(popupPhotoPlace);
-});
-closePopupProfileButton.addEventListener('click',() => {
-  togglePopup(popupProfile);
 });
 closePopupPlaceButton.addEventListener('click',() => {
   togglePopup(popupPhotoPlace);
@@ -145,5 +109,31 @@ closePopupPlaceButton.addEventListener('click',() => {
 closePopupImageButton.addEventListener('click',() => {
   togglePopup(popupPlaceImage);
 });
-formFieldProfile.addEventListener('submit', formSubmitHandler);
+
 formFieldPlace.addEventListener('submit', formPlaceHandler);
+
+formFieldProfile.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  activityProfile.textContent = activityInput.value;
+  togglePopup(popupProfile);
+}
+
+function togglePopup (pop) {
+  pop.classList.toggle('popup__opened');
+}
+
+function upProfileInfo() {
+  nameInput.value = nameProfile.textContent
+  activityInput.value = activityProfile.textContent;
+}
+
+openPopupButton.addEventListener('click',() => {
+  upProfileInfo();
+  togglePopup(popupProfile);
+});
+closePopupProfileButton.addEventListener('click',() => {
+  togglePopup(popupProfile);
+});
